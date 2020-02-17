30e200ef19f941d186a3af6efcdb240f17679630
Chapelle Julien <you@example.comLHP3.cjulien@gmail.com>
2020-02-17 16:57:53 +0100

    exo 1 a 14 terminé, soucis avec tableau d'erreur qui ne fonctionne plus

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php require_once('Controllers/Home/hlm_homeController.php'); var_dump($_POST); var_dump($arrayError)?>
+<?php require_once('Controllers/Home/hlm_homeController.php'); ?>
 <!DOCTYPE html>
 <html lang="fr">
 
